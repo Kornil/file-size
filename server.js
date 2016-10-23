fe024236902878ab7352edcbf901c1fae8d7d57d
Francesco Agnoletto <francesco.agnoletto92@gmail.com>
2016-10-23 12:22:47 +0000

    upload made with multer

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 var express = require('express');
+var multer  = require('multer');
 var app = express();
+app.engine('jade', require('jade').__express);
+var upload = multer({ dest: 'uploads/' });
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
-}).listen(8080);
+    res.render("index.jade");
+});
+
+app.post('/upload', upload.single('file'), function (req, res) {
+    var size = "File size is "+ req.file["size"]+" bytes";
+    res.send(size);
+});
+
+app.listen(8080);
